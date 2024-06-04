4df29ad5d1cb650b7c27f0e4ccf789c378fb255e
Menaka Godakanda <156875412+MenakaGodakanda@users.noreply.github.com>
2024-06-04 14:21:31 +0800

    Create signal_controller.h

---
+++ include/signal_controller.h
@@ -0,0 +1,15 @@
+#ifndef SIGNAL_CONTROLLER_H
+#define SIGNAL_CONTROLLER_H
+
+class Train;  // Forward declaration
+
+class SignalController {
+public:
+    void updateSignals(const Train &train);
+    // Other member functions...
+
+private:
+    // Member variables...
+};
+
+#endif // SIGNAL_CONTROLLER_H
